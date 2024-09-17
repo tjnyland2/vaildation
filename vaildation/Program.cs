b68ad6ae9a91487ccc174d460e4a28343341f7d3
AQ242209 <AQ242209@F31_15>
2024-09-17 13:41:36 +0100

    Add project files.

---
+++ vaildation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaildation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your date of birth");
+            string myBirthday = Console.ReadLine();
+            do
+            { 
+                try
+                {
+                    DateTime TheTimeNow = DateTime.Now;
+                    DateTime myBirth = DateTime.Parse(myBirthday);
+                    if (myBirth < TheTimeNow)
+                    {
+                        Console.WriteLine("Vaid birthday");
+                        Console.ReadLine();
+                    }
+                    else if (myBirth > TheTimeNow)
+                    {
+                        Console.WriteLine("Invaid birthday");
+                        Console.ReadLine();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("This is not a correct DoB!");
+                    Console.ReadLine();
+
+                }
+            }
+            while (0 == 0);
+        }
+    }
+}
